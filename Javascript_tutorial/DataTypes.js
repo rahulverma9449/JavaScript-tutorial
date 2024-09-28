e5ea20e2161af4fc5c85b044d8133a0a9b63d6b0
rahul verma <48601181+rahulverma9449@users.noreply.github.com>
2024-09-28 00:07:25 +0000

    conversation

---
+++ Javascript_tutorial/DataTypes.js
@@ -0,0 +1,15 @@
+"Use Strict"; // treat all JS code as newer version
+
+alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3);  
+
+
+// number = 2
+// bigint = 
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = 
+// symbol
+// object
---
+++ Javascript_tutorial/Variabletype.js
@@ -0,0 +1,7 @@
+Bank_Name = "SBI"
+Account_Id = 123
+AccountEmail = "hcl@gmail.com"
+AccountCity = "Gurgaon"
+AccountPassword = "23244322"
+
+console.table([Bank_Name, Account_Id, AccountEmail, AccountCity, AccountPassword]);
