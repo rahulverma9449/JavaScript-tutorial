bf196c1104e7816143acf8129c82ceda85ce8c1c
rahul verma <48601181+rahulverma9449@users.noreply.github.com>
2024-09-28 00:14:16 +0000

    string

---
+++ Javascript_tutorial/conversionOperation.js
@@ -0,0 +1,27 @@
+
+let score = "33"
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueNum = Number(score)
+console.log(typeof valueNum);
+
+
+let islogged = "rrr"
+ let booleanin = Boolean(islogged)
+ console.log(booleanin);
+
+ // 1 = true; 0 = false
+ // "" = false
+ // "hwh" = true
+
+
+ let str1 = "rahul"
+ let str2 = "verma"
+ let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + "1");
